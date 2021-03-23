022b99450a2bbb3f9b16307703af0eefdf56b166
yangtingting123456 <3048903923@qq.com>
2021-03-23 14:53:35 +0800

    excel读取，类封装

--- course_exercises/excel_read/demo03_read_excel_函数封装.py
+++ course_exercises/excel_read/demo03_read_excel_函数封装.py
@@ -19,17 +19,22 @@ import xlrd
 
 
 #2.对象的封装
+import StudentBaseInfo
 def read_excel_obj(excel_path):
     WorkBook = xlrd.open_workbook(excel_path)
     # #获取工作簿的第一张表
-    sheet01 = WorkBook.sheet_by_name('StudentInfo')
-    all_student_info = []
-    for i in range(1, sheet01.nrows):  # 总行
-        row_student_info = []
-        for j in range(sheet01.ncols):  # 总列
-            row_student_info.append(sheet01.cell_value(i, j))
-        all_student_info.append(row_student_info)
-    return all_student_info
+    sheet = WorkBook.sheet_by_name('StudentInfo')
+    all_student_obj = []
+    # -----方法1：获取每个单元格然后赋值
+    # -----方法2：循环获取保存
+    for i in range(1, sheet.nrows):
+        students = StudentBaseInfo.StudentBaseInf(sheet.cell_value(i, 0),
+                                                  sheet.cell_value(i, 1),
+                                                  sheet.cell_value(i, 2),
+                                                  sheet.cell_value(i, 3),
+                                                  sheet.cell_value(i, 4))
+        all_student_obj.append(students)
+    return all_student_obj
 
 current_path = os.path.dirname(__file__)
 excel_path = os.path.join(current_path, 'data/StudentInfo.xlsx')
---
+++ course_exercises/excel_read/demo04_read_excel_类封装.py
@@ -0,0 +1,38 @@
+import os,xlrd
+import StudentBaseInfo
+class ReadExcel:
+    def __init__(self,excel_path,sheet_name):
+        self.excel_path = excel_path
+        self.sheet_name = sheet_name
+    # 读取excel中的数据制作成列表，对象的封装
+    def read_excel_list(self):
+        WorkBook = xlrd.open_workbook(self.excel_path)
+        sheet01 = WorkBook.sheet_by_name(self.sheet_name)
+        all_student_info = []
+        for i in range(1, sheet01.nrows):  # 总行
+            row_student_info = []
+            for j in range(sheet01.ncols):  # 总列
+                row_student_info.append(sheet01.cell_value(i, j))
+            all_student_info.append(row_student_info)
+        return all_student_info
+    # 2.对象的封装
+    def read_excel_obj(self):
+        WorkBook = xlrd.open_workbook(self.excel_path)
+        sheet = WorkBook.sheet_by_name(self.sheet_name)
+        all_student_obj = []
+        for i in range(1, sheet.nrows):
+            students = StudentBaseInfo.StudentBaseInf(sheet.cell_value(i, 0),
+                                                      sheet.cell_value(i, 1),
+                                                      sheet.cell_value(i, 2),
+                                                      sheet.cell_value(i, 3),
+                                                      sheet.cell_value(i, 4))
+            all_student_obj.append(students)
+        return all_student_obj
+#测试
+if __name__ == '__main__':
+    current_path = os.path.dirname(__file__)
+    excel_path = os.path.join(current_path, 'data/StudentInfo.xlsx')
+    RD_excel = ReadExcel(excel_path,'StudentInfo')
+    print(RD_excel.read_excel_list())
+    for i in range(len(RD_excel.read_excel_obj())):
+        print(RD_excel.read_excel_obj()[i].name)
