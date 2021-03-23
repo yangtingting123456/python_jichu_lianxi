c8176dad4c4118bda6b54917d6c3bd2bfe32b9f5
yangtingting123456 <3048903923@qq.com>
2021-03-23 17:55:36 +0800

    write logs second

--- course_exercises/20210321/utils/log_utils.py
+++ course_exercises/20210321/utils/log_utils.py
@@ -4,7 +4,17 @@ conf = ConfigUtils()
 log_path = conf.get_log_path()
 class LogUtils:
     def __init__(self,log_path):
-        self.log_path = log_path
-        self.logger = logging.getLogger()
+        self.log_path = log_pathlogger
+        logger= logging.getLogger('python实战日志')
+        #设置日志级别 :  DEBUG<INFO<WARNING<ERROR<CRITICAL
+        logger.setLevel(level=logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s - %(message)s')
+        file_log.setFormatter(formatter)
+        # 将日志写入文件中
+        logger.addHandler(file_log)
 
-    def set_log
+    def set_log_leve(self):
+        self.logger.setLevel(level=logging.info())
+
+    def set_log_format(self):
+        self.logger
